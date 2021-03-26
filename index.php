101365630a544bfed133d2ee76220e130a98c2c2
mabuselaaya <27950608+mabuselaaya@users.noreply.github.com>
2021-03-26 17:21:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -41,19 +41,19 @@
 				<li id="instagram"><a href="https://www.instagram.com/khwezi.morningstar/"><i class="fa fa-instagram"></i></a></li>
 			</ul>
 		</div>
-		
+		          <p class="col-12 text-black text-center tm-copyright-text">
+            Copyright &copy; 2021 Aodevsolutions App. 
+            Designed by <a href="https://aodevsolutions.com/" class="tm-copyright-link">AODS</a>
+          </p>
 		
 	</div>
 	<div class="watch-section"><img src="images/watch.png" /></div>
 </div>
 
 <!-- Page footer -->
-        <footer class="row">
-          <p class="col-12 text-black text-center tm-copyright-text">
-            Copyright &copy; 2021 Aodevsolutions App. 
-            Designed by <a href="https://aodevsolutions.com/" class="tm-copyright-link">AODS</a>
-          </p>
-</footer>
+        
+
+
 
 </body>
 </html>
